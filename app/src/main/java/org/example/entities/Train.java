102ab751c9f87a99986d731d53b50bbb95599b42
rishipandey2 <rishipandeyinfo@gmail.com>
2025-01-18 09:07:32 +0530

    Java project

---
+++ app/src/main/java/org/example/entities/Train.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+import org.checkerframework.checker.units.qual.Time;
+
+import java.util.List;
+import java.util.Map;
+
+public class Train {
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private Map<String,Time> stationTimes;
+    private List<String> stations;
+}
